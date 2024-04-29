ff34976fa417ff6d3ada2ce6429b3a178ef63a16
Jerry Young <62739108+jackthepanda96@users.noreply.github.com>
2024-04-29 16:05:37 +0700

    Update base URL

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -22,7 +22,7 @@ import ProfileMentee from "../pages/Mentee/ProfileMentee";
 import ProfileMentor from "../pages/Mentor/ProfileMentor";
 import DetailTask from "../pages/Mentor/DetailTask";
 
-axios.defaults.baseURL = "https://mentutor.altapro.online/";
+axios.defaults.baseURL = "https://mentutor.firdauslabs.com/";
 
 const index = () => {
   const dispatch = useDispatch();
